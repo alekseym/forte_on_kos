b984795e04fc260dbd45fa891361d281bf1573fb
Alex Maksimov <almaximort@yandex.ru>
2022-07-21 12:26:12 +0300

    Temporary implementation of fboot loading

---
+++ common/common.h
@@ -0,0 +1,10 @@
+/* © 2021 AO Kaspersky Lab */
+#ifndef FORTE_COMMON_H
+#define FORTE_COMMON_H
+
+#define USER_PARTITION 1U
+#define CERTIFICATE_PARTITION 2U
+
+int MountFileSystem(unsigned partitionId);
+
+#endif
